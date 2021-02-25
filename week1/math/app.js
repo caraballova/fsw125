ebc326980bb05f680bed3d84e768dafc47cc3976
caraballova <vanessa.caraballo@bryanuniversity.edu>
2021-02-25 00:10:26 -0500

    Node-Module-Basics

---
+++ week1/math/app.js
@@ -0,0 +1,10 @@
+const {add, minus, multiply, divide} = require("./math.js")
+
+console.log(add(15, 30))
+
+console.log(minus(30, 15))
+
+console.log(multiply(31, 25))
+
+console.log(divide(20, 2))
+
